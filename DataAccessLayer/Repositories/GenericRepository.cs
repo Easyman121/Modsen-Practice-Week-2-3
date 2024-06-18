20dda7523ffa3d2331141b9668c21cf882631f54
eugenia.majako <eugenia.majako@outlook.com>
2024-06-18 21:57:03 +0300

    added generic repository

---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected DbContext context;
+
+        public GenericRepository(DbContext context)
+        {
+            this.context = context; 
+        }
+
+        public async Task<TEntity> GetById(int id) => await context.Set<TEntity>().FindAsync(id).AsTask();
+
+        public async Task<List<TEntity>> GetAll() => await context.Set<TEntity>().ToListAsync();
+
+        public async Task Insert(TEntity entity)
+        {
+            await context.Set<TEntity>().AddAsync(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Update(TEntity entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+        }
+        
+        public async Task Delete(TEntity entity)
+        {
+            context.Set<TEntity>().Remove(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetById(int id);
+        Task<List<TEntity>> GetAll();
+        Task Insert(TEntity entity);
+        Task Delete(TEntity entity);
+        Task Update(TEntity entity);
+    }
+}
