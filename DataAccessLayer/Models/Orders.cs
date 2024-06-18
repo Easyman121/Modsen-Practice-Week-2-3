aee749b07b0638385c062e50103c17ac33f77cbe
Daniil <atlas21022004@gmail.com>
2024-06-18 17:14:32 +0300

    Created models for Categories, Orders, OrderItems, Products and Users

---
+++ DataAccessLayer/Models/Orders.cs
@@ -0,0 +1,9 @@
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+namespace DataAccessLayer.Models;
+
+public class Orders
+{
+    public int Id { get; set; }
+    public DateTime DateTime { get; set; }
+    public Users UserId { get; set; }
+}
---
+++ DataAccessLayer/Models/Users.cs
@@ -0,0 +1,10 @@
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+namespace DataAccessLayer.Models;
+
+public class Users
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public int PasswordHash { get; set; }
+    public string Email { get; set; }
+}
