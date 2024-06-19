387dc2dfa076358d50215b1ddb766774e7601377
Anastasia Panasevich <matildaweller66@gmail.com>
2024-06-19 19:38:19 +0300

    User validation (+ service just for testing)

---
+++ BusinessLogicLayer/Services/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLayer.Models;
+using FluentValidation.Results;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<ValidationResult> CreateUserAsync(Users user);
+        // other methods?
+    }
+}
---
+++ BusinessLogicLayer/Validators/UserValidator.cs
@@ -0,0 +1,56 @@
+﻿using FluentValidation;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Validators
+{
+    public class UserValidator : AbstractValidator<Users>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.UserName)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty().WithMessage("{PropertyName} is empty!")
+                .Length(2, 50).WithMessage("Your {PropertyName} length of {TotalLength} is beyond the acceptable range from 2 to 50")
+                .Must(BeAValidUsername).WithMessage("{PropertyName} contains invalid characters!");
+
+            RuleFor(u => u.Email)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty().WithMessage("{PropertyName} is empty!")
+                .EmailAddress().WithMessage("Invalid {PropertyName} format!");
+
+            // TO DO: make a proper hash validation
+            RuleFor(u => u.PasswordHash)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotEmpty().WithMessage("{PropertyName} is empty!")
+                .GreaterThan(0).WithMessage("{PropertyName} should be greater than 0");
+        }
+
+        protected bool BeAValidUsername(string username)
+        {
+            username = username.Replace("_", "").Replace("-", "");
+            //checks in unicode
+            return username.All(char.IsLetterOrDigit);
+        }
+    }
+}
+/*
+
+BindingList <strin> errors = new BindingList <string>();
+
+UserValidator validator = new UserValidator();
+
+ValidationResult results = validator.Validate(user);
+
+if(results.IsValid == false)
+{
+    foreach (ValidationFailure failure in results.Errors)
+    {
+        errors.Add(failure.ErrorMessage);
+    }
+}
+ */
