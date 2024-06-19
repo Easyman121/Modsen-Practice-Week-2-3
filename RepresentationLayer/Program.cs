10e175653197f8bfc50ba4a85648ff21b5fa8c8a
Daniil <atlas21022004@gmail.com>
2024-06-19 13:14:02 +0300

    Added missing dependencies and added DbContext to ApplicationBuilder

--- RepresentationLayer/Program.cs
+++ RepresentationLayer/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataAccessLayer.AppContext>(options =>
+{
+    options.UseSqlServer("Server=ATLAS\\SQLEXPRESS;Database=ModsenPractice");
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
