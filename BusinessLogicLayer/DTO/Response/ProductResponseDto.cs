4043d192541a71b0efdcdd53189661e5254c1bfa
Anastasia Panasevich <matildaweller66@gmail.com>
2024-06-21 12:14:33 +0300

    Changed decimal Price to double in DTO

--- BusinessLogicLayer/DTO/Response/ProductResponseDto.cs
+++ BusinessLogicLayer/DTO/Response/ProductResponseDto.cs
@@ -11,7 +11,7 @@ namespace BusinessLogicLayer.DTO.Response
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public decimal Price { get; set; }
+        public double Price { get; set; }
         public string CategoryName { get; set; }
     }
 }
