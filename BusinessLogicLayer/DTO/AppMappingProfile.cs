5230b492d6924fa894d5f3cb3ea00b0842c3322a
efunim <eugenia.majako@gmail.com>
2024-06-27 18:30:25 +0300

    explicitly specified the matched fields in mappers

--- BusinessLogicLayer/DTO/AppMappingProfile.cs
+++ BusinessLogicLayer/DTO/AppMappingProfile.cs
@@ -10,17 +10,56 @@ namespace BusinessLogicLayer.DTO
     {
         public AppMappingProfile() 
         {
-            CreateMap<CategoryRequestDto, Category>();
-            CreateMap<OrderItemRequestDto, OrderItem>();
-            CreateMap<OrderRequestDto, Order>();
-            CreateMap<ProductRequestDto, Product>();
-            CreateMap<UserRequestDto, User>();
+            CreateMap<CategoryRequestDto, Category>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
 
-            CreateMap<Category, CategoryResponseDto>();
-            CreateMap<OrderItem, OrderItemResponseDto>();
-            CreateMap<Order, OrderResponseDto>();
-            CreateMap<Product, ProductResponseDto>();
-            CreateMap<User, UserResponseDto>();
+            CreateMap<OrderItemRequestDto, OrderItem>()
+                .ForMember(dest => dest.OrderId, opt => opt.MapFrom(src => src.OrderId))
+                .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.ProductId))
+                .ForMember(dest => dest.Count, opt => opt.MapFrom(src => src.Count));
+
+            CreateMap<OrderRequestDto, Order>()
+                .ForMember(dest => dest.DateTime, opt => opt.MapFrom(src => src.DateTime))
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId));
+
+            CreateMap<ProductRequestDto, Product>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+                .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price))
+                .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.CategoryId));
+
+            CreateMap<UserRequestDto, User>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
+                .ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => src.PasswordHash))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
+
+            CreateMap<Category, CategoryResponseDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
+
+            CreateMap<OrderItem, OrderItemResponseDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.OrderId, opt => opt.MapFrom(src => src.OrderId))
+                .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.ProductId))
+                .ForMember(dest => dest.Count, opt => opt.MapFrom(src => src.Count));
+
+            CreateMap<Order, OrderResponseDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.DateTime, opt => opt.MapFrom(src => src.DateTime))
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
+                .ForMember(dest => dest.Items, opt => opt.Ignore());
+
+            CreateMap<Product, ProductResponseDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+                .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price))
+                .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.CategoryId));
+
+            CreateMap<User, UserResponseDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email));
         }
     }
 }
