19a195e28dca5de1d63461fb80fab7ce913a6e91
efunim <eugenia.majako@gmail.com>
2024-06-27 12:46:49 +0300

    changed DTO's

--- BusinessLogicLayer/DTO/Response/OrderResponseDto.cs
+++ BusinessLogicLayer/DTO/Response/OrderResponseDto.cs
@@ -6,6 +6,5 @@ public class OrderResponseDto
     public int Id { get; set; }
     public DateTime DateTime { get; set; }
     public int UserId { get; set; }
-    public string UserName { get; set; }
     public List<OrderItemResponseDto> Items { get; set; }
 }
